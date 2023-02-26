8fd677ac6db1b7a9020f9755a6eaefeb20e600a5
AxelMagram <106857882+AxelMagram@users.noreply.github.com>
2023-02-26 20:24:49 -0300

    Add files via upload

---
+++ cvfinal.js
@@ -0,0 +1,21 @@
+
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+document.getElementById("boton_color").addEventListener ("click" , function(){
+    document.body.style.backgroundColor = '#00ff99' ; 'break'
+});
+
+
+
+    document.getElementById("boton_color").addEventListener ("click" , function(){
+  document.body.style.backgroundColor = '##ccff33' ; 'break'
+});  
+
